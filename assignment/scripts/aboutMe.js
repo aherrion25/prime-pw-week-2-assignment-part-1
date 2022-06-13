e9c4a1304bc5998d6e234bbf89659597bd1f1287
Aubree Herrion <aubreeherrion@gmail.com>
2022-06-13 12:34:58 -0500

    Completed the stretch goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -84,6 +84,20 @@ if ( pets > allowedPets) {
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
 
+let mostPets = 5;
+pets = 5;
+friendsPets = 8;
+ if(mostPets == pets){
+     console.log(pets);
+ }
+ else if (mostPets == friendsPets) {
+     console.log(friendsPets);
+ }
+ else{
+     console.log('no pets available');
+
+ }
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
@@ -91,6 +105,32 @@ if ( pets > allowedPets) {
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+switch (luckyNumber = 4) {
+    case luckyNumber = 1:
+        console.log('First is the worst');
+        break;
+        case luckyNumber = 2:
+            console.log('Second is the best');
+        break;
+        case luckyNumber = 3:
+            console.log('Third is the one with the polka dot dress');
+
+    default:
+        console.log('Luck is what happens when preparation meets opportunity');
+        break;
+}
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+
+adventurous = false
+
+function areYou(adventurous) {
+    return (adventurous ? 'Adventures are great!' : 'How about we stay home?')
+}
+
+console.log(areYou(true));
+
+console.log(areYou(false));
+
+console.log(areYou(null));
 
 
